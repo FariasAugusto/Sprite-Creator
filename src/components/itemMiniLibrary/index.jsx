b62144d10f43f25e4247c5e22adee3c7ca20fa7a
FariasAugusto <fugustoarias@gmail.com>
2022-12-14 19:08:00 -0300

    componetização

---
+++ src/components/itemMiniLibrary/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ItemMiniLibrary = (props) => (
+
+    <a>
+        Ver Tudo...
+    </a>
+
+);
+
+export default ItemMiniLibrary
---
+++ src/components/miniLibrary/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ItemMiniLibrary from '/.itemMiniLibrary';
+
+const MiniLibrary = (props) => (
+    
+    <ItemMiniLibrary><ItemMiniLibrary/>
+    
+    <ItemMiniLibrary><ItemMiniLibrary/>
+
+);
+
+export default MiniLibrary;
