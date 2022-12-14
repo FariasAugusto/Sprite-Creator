2483d149751ed8001689b1ad5b367e5f12971c82
Thayna Saraiva <thaynasaraiva99@gmail.com>
2022-12-14 18:20:42 -0300

    Início da componentização

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = (props) => (
+
+    <footer>
+
+        <ItemFooter>Ajuda</ItemFooter>
+
+        <ItemFooter>Contato</ItemFooter>
+
+        <ItemFooter>Idiomas</ItemFooter>
+
+        <a classname='atalhosFooter' href=''>
+            Sobre Nós
+        </a> {/*  Substituir o ultimo componente */}
+
+    </footer>
+    
+)
+
+export default Footer;
