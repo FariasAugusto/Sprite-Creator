8d271b7bc88ca44e83a708ab550911526a40c6de
Thayna Saraiva <thaynasaraiva99@gmail.com>
2022-12-21 19:23:34 -0300

    instructions

--- src/components/instructions/index.jsx
+++ src/components/instructions/index.jsx
@@ -1,56 +1,72 @@
 import React from 'react';
 import './instructions.css';
 
-const instructions = (props) => (
-    <div>
-            <div classname='passoapasso'>
+const Instructions = (props) => (
+    
+    <Instructions>
+        
+        <ItemInstrucoes></ItemInstrucoes>
 
-                <h3 classname='tituloPassoaPasso'>
-                    Procure...
-                </h3>
+        <ItemInstrucoes></ItemInstrucoes>
 
-                <img src='public\enfeiteLupa.png' alt='Ilustração Passo I' />
+        <ItemInstrucoes></ItemInstrucoes>
 
-                <p classname='explicacaoPassoaPasso'>
-                    Aqui no SpriteCreator, existem várias pessoas que diaramente criam seus Sprites e postam aqui para todos usarem! 
-                    Procure com jeitinho, que você com certeza encontrará o que quiser!
-                </p>
+    </Instructions>
+
+/* <TituloInstrucoes></TituloInstrucoes>
+
+<ImagemInstrucoes></ImagemInstrucoes>
+
+<TextoInstrucoes></TextoInstrucoes> */
+    // <div>
+    //         <div classname='passoapasso'>
+
+    //             <h3 classname='tituloPassoaPasso'>
+    //                 Procure...
+    //             </h3>
+
+    //             <img src='public\enfeiteLupa.png' alt='Ilustração Passo I' />
+
+    //             <p classname='explicacaoPassoaPasso'>
+    //                 Aqui no SpriteCreator, existem várias pessoas que diaramente criam seus Sprites e postam aqui para todos usarem! 
+    //                 Procure com jeitinho, que você com certeza encontrará o que quiser!
+    //             </p>
 
 
-            </div>
+    //         </div>
 
-            <div classname='passoapasso'>
+    //         <div classname='passoapasso'>
 
-                <h3 classname='tituloPassoaPasso'>
-                    Crie...
-                </h3>
+    //             <h3 classname='tituloPassoaPasso'>
+    //                 Crie...
+    //             </h3>
 
-                <img src='public\enfeiteCriacao.png' alt='Ilustração Passo II' />
+    //             <img src='public\enfeiteCriacao.png' alt='Ilustração Passo II' />
 
-                <p classname='explicacaoPassoaPasso'>
-                    Se você não quiser usar Sprites de outros, ou não está achando o que deseja. Crie seu Sprite! Nosso sistema 
-                    de criação de Sprites é bem divertido, de Sprites é bem divertido, você se sentirá criando um avatar em um videogame! 
-                    E assim que você o criar, nós automaticamente criaremos todas as animações dele pra você. De nada!
-                </p>
+    //             <p classname='explicacaoPassoaPasso'>
+    //                 Se você não quiser usar Sprites de outros, ou não está achando o que deseja. Crie seu Sprite! Nosso sistema 
+    //                 de criação de Sprites é bem divertido, de Sprites é bem divertido, você se sentirá criando um avatar em um videogame! 
+    //                 E assim que você o criar, nós automaticamente criaremos todas as animações dele pra você. De nada!
+    //             </p>
 
-            </div>
+    //         </div>
 
-            <div classname='passoapasso'>
+    //         <div classname='passoapasso'>
 
-                <h3 classname='tituloPassoaPasso'>
-                    Use!
-                </h3>
+    //             <h3 classname='tituloPassoaPasso'>
+    //                 Use!
+    //             </h3>
 
-                <img src='public\enfeiteSorriso.png' alt='Ilustração Passo III' />
+    //             <img src='public\enfeiteSorriso.png' alt='Ilustração Passo III' />
 
-                <p classname='explicacaoPassoaPasso'>
-                    Assim que você tiver tudo, comece a criar seu jogo! Use os Sprites o quanto você quiser, são todos 
-                    de graça e não tem nenhum problema você enfeitar seus lindos jogos com os nossos belíssimos Sprites!
-                </p>
+    //             <p classname='explicacaoPassoaPasso'>
+    //                 Assim que você tiver tudo, comece a criar seu jogo! Use os Sprites o quanto você quiser, são todos 
+    //                 de graça e não tem nenhum problema você enfeitar seus lindos jogos com os nossos belíssimos Sprites!
+    //             </p>
 
-            </div>
+    //         </div>
 
-        </div>
+    //     </div>
 ); 
 
-export default instructions;
+export default Instructions;
